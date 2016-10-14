09f75cec0a8cee4051d79cc5b001b13a757b369d
Tarundeep Singh <virk369@gmail.com>
2016-10-14 02:33:24 -0700

    Add project files.

---
+++ Lesson6-COMP2084/hello.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Week 4-Hello" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="hello.aspx.cs" Inherits="Lesson6_COMP2084.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<h1>Hello</h1>
+   <div> <asp:label id="lblMessage" runat="server" cssclass="alert alert-info" visible="false"></asp:label> </div>
+    <asp:textbox id="txtname" runat="server" placeholder="Enter Your Name"></asp:textbox>
+    <asp:button id="btnsubmit" runat="server" text="submit" cssclass="btn btn-success" OnClick="btnsubmit_Click" />
+</asp:Content>
---
+++ Lesson6-COMP2084/tip.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lesson6_COMP2084
+{
+    public partial class tip : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            /* get amount from the textbox
+             * save in a variable of double (which hold decimals)
+             * first we have to convert the value from a string to a double
+             */
+            Double amount = Convert.ToDouble(txtAmount.Text);
+
+            // get tip percent
+            Double tipPercent = Convert.ToDouble(ddlPercent.SelectedValue);
+
+            // calculate the tip and total
+            Double tipAmount = amount * tipPercent;
+            Double total = amount + tipAmount;
+
+            // display result
+            lblTip.Text = tipAmount.ToString();
+            lblTotal.Text = total.ToString();
+        }
+    }
+}
